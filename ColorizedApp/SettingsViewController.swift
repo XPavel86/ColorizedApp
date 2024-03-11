7cdc8823abda6a4a41fb3fa79a5fe5be423157b4
Pavel <rezusx@mail.ru>
2024-03-11 18:21:34 +0530

    fix

--- ColorizedApp/SettingsViewController.swift
+++ ColorizedApp/SettingsViewController.swift
@@ -32,10 +32,10 @@ class SettingsViewController: UIViewController {
     // MARK: - View life cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         let tap = UITapGestureRecognizer(target: self, action: #selector(hideKeyboard))
-           tap.cancelsTouchesInView = false
-           view.addGestureRecognizer(tap)
+        tap.cancelsTouchesInView = false
+        view.addGestureRecognizer(tap)
         
         [redTextField, greenTextField, blueTextField].forEach {
             $0?.delegate = self
@@ -51,7 +51,7 @@ class SettingsViewController: UIViewController {
     // MARK: - IB Actions
     @IBAction func actionSlider(slider sender: UISlider) {
         let value = String(format: "%.2f", sender.value)
-
+        
         switch sender {
         case redSlider:
             redValue.text = value
@@ -66,21 +66,21 @@ class SettingsViewController: UIViewController {
             blueTextField.text = value
         }
         
-            color = ColorRGB(
-                red: redSlider.value,
-                green: greenSlider.value,
-                blue: blueSlider.value
-            )
-
-            colorView.backgroundColor = UIColor(
-                red: CGFloat(redSlider.value),
-                green: CGFloat(greenSlider.value),
-                blue: CGFloat(blueSlider.value),
-                alpha: 1.0
-            )
+        color = ColorRGB(
+            red: redSlider.value,
+            green: greenSlider.value,
+            blue: blueSlider.value
+        )
+        
+        colorView.backgroundColor = UIColor(
+            red: CGFloat(redSlider.value),
+            green: CGFloat(greenSlider.value),
+            blue: CGFloat(blueSlider.value),
+            alpha: 1.0
+        )
     }
     
-    @IBAction func closeAction() {
+    @IBAction func donePressed() {
         hideKeyboard()
         delegate?.setColor(color: color)
         dismiss(animated: true)
@@ -88,43 +88,43 @@ class SettingsViewController: UIViewController {
     
     // MARK: - Exported methods
     @objc func hideKeyboard() {
-           view.endEditing(true)
-       }
+        view.endEditing(true)
+    }
 }
 
-    // MARK: - Extensions
+// MARK: - Extensions
 extension SettingsViewController: UITextFieldDelegate {
     
     func setupUI() {
-     
-        redSlider.minimumTrackTintColor = .red
-        greenSlider.minimumTrackTintColor = .green
-        blueSlider.minimumTrackTintColor = .blue
-            
-        redSlider.value = color.red
-        greenSlider.value = color.green
-        blueSlider.value = color.blue
-  
-        [redSlider, greenSlider, blueSlider].forEach { slider in
-            slider?.addTarget(self, action: #selector(actionSlider(slider:)), for: .valueChanged)
- 
-            actionSlider(slider: slider!)
+        let colors: [UIColor] = [.red, .green, .blue]
+        let sliders: [UISlider?] = [redSlider, greenSlider, blueSlider]
+        let colorValues = [color.red, color.green, color.blue]
+        
+        for (index, slider) in sliders.enumerated() {
+            if let slider = slider {
+                slider.value = colorValues[index]
+                actionSlider(slider: slider)
+                slider.minimumTrackTintColor = colors[index]
+            }
         }
     }
-
+  
     func textFieldDidEndEditing(_ textField: UITextField) {
-         
+        
         if let text = textField.text, let value = Float(text) {
-
             switch textField {
-                case redTextField: redSlider.value = value
-                case greenTextField: greenSlider.value = value
-                default: blueSlider.value = value
-           }
-            
-            [redSlider, greenSlider, blueSlider].forEach {
-                slider in
-                actionSlider(slider: slider) }
+            case redTextField:
+                redSlider.setValue(value, animated: true)
+                actionSlider(slider: redSlider)
+                
+            case greenTextField:
+                greenSlider.setValue(value, animated: true)
+                actionSlider(slider: greenSlider)
+                
+            default:
+                blueSlider.setValue(value, animated: true)
+                actionSlider(slider: blueSlider)
+            }
         }
     }
     
@@ -140,37 +140,37 @@ extension SettingsViewController: UITextFieldDelegate {
         // Заменяем запятую на точку
         var newText = (textField.text! as NSString).replacingCharacters(in: range, with: string)
         newText = newText.replacingOccurrences(of: ",", with: ".")
-
+        
         // Проверяем, что в тексте нет двух точек подряд
         if newText.contains("..") {
             return false
         }
- 
+        
         // Проверяем, что в тексте не более одной точки
         let numberOfDots = newText.components(separatedBy: ".").count - 1
         if numberOfDots > 1 && string == "." {
             return false
         }
-
+        
         // Проверяем, что вводимый символ - цифра, точка или запятая
         let allowedCharacterSet = CharacterSet(charactersIn: "0123456789.,")
         let replacementStringCharacterSet = CharacterSet(charactersIn: string)
         guard allowedCharacterSet.isSuperset(of: replacementStringCharacterSet) else {
             return false
         }
-
+        
         // Проверяем длину вводимого текста
         let components = newText.components(separatedBy: CharacterSet(charactersIn: "."))
         let numberOfDigits = components.reduce(0) { $0 + $1.count }
         if numberOfDigits > 3 {
             return false
         }
-
+        
         // Преобразуем текст в число и проверяем, что оно находится в диапазоне от 0 до 1
         if let value = Float(newText), !(0...1).contains(value) {
             return false
         }
-
+        
         textField.text = newText
         return false
     }
