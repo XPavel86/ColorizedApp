8df068a3ef37245555865e2b4c3f04cd275d46d3
Pavel <rezusx@mail.ru>
2024-03-11 13:10:33 +0530

    Initial Commit

---
+++ ColorizedApp/ColorData.swift
@@ -0,0 +1,30 @@
+//
+//  DataColor.swift
+//  ColorizedApp
+//
+//  Created by Pavel Dolgopolov on 10.03.2024.
+//
+
+import Foundation
+
+struct ColorRGB: CustomStringConvertible {
+    var description: String {
+            return "(\(red), \(green), \(green))"
+        }
+    
+    var red: Float
+    var green: Float
+    var blue: Float
+    
+    init(red: Float, green: Float, blue: Float) {
+            self.red = red
+            self.green = green
+            self.blue = blue
+        }
+    
+    init(_ rgb: (Float, Float, Float)) {
+            self.red = rgb.0
+            self.green = rgb.1
+            self.blue = rgb.2
+        }
+}
---
+++ ColorizedApp/SettingsViewController.swift
@@ -0,0 +1,190 @@
+//
+//  ViewController.swift
+//  HW2-3
+//
+//  Created by Pavel Dolgopolov on 15.02.2024.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+    
+    // MARK: - IB Outlets
+    @IBOutlet var colorView: UIView!
+    
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    
+    @IBOutlet var redValue: UILabel!
+    @IBOutlet var greenValue: UILabel!
+    @IBOutlet var blueValue: UILabel!
+    
+    @IBOutlet var redTextField: UITextField!
+    @IBOutlet var greenTextField: UITextField!
+    @IBOutlet var blueTextField: UITextField!
+    
+    // MARK: - Public Properties
+    var color = ColorRGB(red: 0.1, green: 0.3, blue: 0.5)
+    
+    weak var delegate: SettingsViewControllerDelegate?
+    
+    // MARK: - View life cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        let tap = UITapGestureRecognizer(target: self, action: #selector(hideKeyboard))
+           tap.cancelsTouchesInView = false
+           view.addGestureRecognizer(tap)
+        
+        [redTextField, greenTextField, blueTextField].forEach {
+            $0?.delegate = self
+            $0?.setSettings()
+        }
+        setupUI()
+    }
+    
+    override func viewWillLayoutSubviews() {
+        colorView.layer.cornerRadius = 20
+    }
+    
+    // MARK: - IB Actions
+    @IBAction func actionSlider(slider sender: UISlider) {
+        let value = String(format: "%.2f", sender.value)
+
+        switch sender {
+        case redSlider:
+            redValue.text = value
+            redTextField.text = value
+            
+        case greenSlider:
+            greenValue.text = value
+            greenTextField.text = value
+            
+        default:
+            blueValue.text = value
+            blueTextField.text = value
+        }
+        
+            color = ColorRGB(
+                red: redSlider.value,
+                green: greenSlider.value,
+                blue: blueSlider.value
+            )
+
+            colorView.backgroundColor = UIColor(
+                red: CGFloat(redSlider.value),
+                green: CGFloat(greenSlider.value),
+                blue: CGFloat(blueSlider.value),
+                alpha: 1.0
+            )
+    }
+    
+    @IBAction func closeAction() {
+        hideKeyboard()
+        delegate?.setColor(color: color)
+        dismiss(animated: true)
+    }
+    
+    // MARK: - Exported methods
+    @objc func hideKeyboard() {
+           view.endEditing(true)
+       }
+}
+
+    // MARK: - Extensions
+extension SettingsViewController: UITextFieldDelegate {
+    
+    func setupUI() {
+     
+        redSlider.minimumTrackTintColor = .red
+        greenSlider.minimumTrackTintColor = .green
+        blueSlider.minimumTrackTintColor = .blue
+            
+        redSlider.value = color.red
+        greenSlider.value = color.green
+        blueSlider.value = color.blue
+  
+        [redSlider, greenSlider, blueSlider].forEach { slider in
+            slider?.addTarget(self, action: #selector(actionSlider(slider:)), for: .valueChanged)
+ 
+            actionSlider(slider: slider!)
+        }
+    }
+
+    func textFieldDidEndEditing(_ textField: UITextField) {
+         
+        if let text = textField.text, let value = Float(text) {
+
+            switch textField {
+                case redTextField: redSlider.value = value
+                case greenTextField: greenSlider.value = value
+                default: blueSlider.value = value
+           }
+            
+            [redSlider, greenSlider, blueSlider].forEach {
+                slider in
+                actionSlider(slider: slider) }
+        }
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+    }
+    
+    func textField(
+        _ textField: UITextField,
+        shouldChangeCharactersIn range: NSRange,
+        replacementString string: String
+    ) -> Bool {
+        // Заменяем запятую на точку
+        var newText = (textField.text! as NSString).replacingCharacters(in: range, with: string)
+        newText = newText.replacingOccurrences(of: ",", with: ".")
+
+        // Проверяем, что в тексте нет двух точек подряд
+        if newText.contains("..") {
+            return false
+        }
+
+        // Проверяем, что в тексте не более одной точки
+        let numberOfDots = newText.components(separatedBy: ".").count - 1
+        if numberOfDots > 1 && string == "." {
+            return false
+        }
+
+        // Проверяем, что вводимый символ - цифра, точка или запятая
+        let allowedCharacterSet = CharacterSet(charactersIn: "0123456789.,")
+        let replacementStringCharacterSet = CharacterSet(charactersIn: string)
+        guard allowedCharacterSet.isSuperset(of: replacementStringCharacterSet) else {
+            return false
+        }
+
+        // Проверяем длину вводимого текста
+        let components = newText.components(separatedBy: CharacterSet(charactersIn: "."))
+        let numberOfDigits = components.reduce(0) { $0 + $1.count }
+        if numberOfDigits > 3 {
+            return false
+        }
+
+        // Преобразуем текст в число и проверяем, что оно находится в диапазоне от 0 до 1
+        if let value = Float(newText), !(0...1).contains(value) {
+            return false
+        }
+
+        textField.text = newText
+        return false
+    }
+}
+
+extension UITextField {
+    
+    func setSettings() {
+        autocorrectionType = .no
+        smartQuotesType = .no
+        smartDashesType = .no
+        smartInsertDeleteType = .no
+        keyboardType = .decimalPad
+        textContentType = .oneTimeCode
+        inputAccessoryView = nil
+    }
+}
